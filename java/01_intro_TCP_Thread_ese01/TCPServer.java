c6d2aeb968626eb298aff12797c6eb0f2fb7ba75
Paolo <Prof@foo.foo>
2023-01-10 23:27:27 +0100

    modificato con thread

--- java/01_intro_TCP_Thread_ese01/TCPServer.java
+++ java/01_intro_TCP_Thread_ese01/TCPServer.java
@@ -2,25 +2,48 @@ import java.io.*;
 import java.net.*;
 
 class TCPServer {
- public static void main(String argv[]) throws Exception {
-  System.out.println("Hello I'm SERVER");
+  public static void main(String argv[]) throws Exception {
+    System.out.println("Hello I'm SERVER");
 
-  ServerSocket serverSocket = new ServerSocket(6789);
+    ServerSocket serverSocket = new ServerSocket(6789);
 
-  while (true) {
-   Socket connectionSocket = serverSocket.accept();
-   System.out.println("oh! Ho una connessione!!");
+    while (true) {
 
-   DataInputStream dis=new DataInputStream(connectionSocket.getInputStream());  
-   String  str=(String)dis.readUTF();
-     
-   System.out.println(connectionSocket.getInetAddress().toString()+ ":" + connectionSocket.getPort() + " message= "+str);  
-   
-   Tool.pressEnterKeyToContinue("s1 ) premi enter chiudere la connessione con il client ");
-   
-   connectionSocket.close();
-   System.out.println("Ho chiuso la connessione !! ");
-   
+     Socket connectionSocket = serverSocket.accept();
+     new connectionConUnClient(connectionSocket).start(); 
+  
+    }
   }
- }
+}
+
+
+class connectionConUnClient extends Thread{
+  private Socket connectionSocket ;
+    
+  public  connectionConUnClient( Socket s ){
+    this.connectionSocket = s;
+  };
+
+  public void run(){
+
+    System.out.println("oh! Ho una connessione!!");
+    try {
+      DataInputStream dis=new DataInputStream(connectionSocket.getInputStream());  
+      String  str=(String)dis.readUTF();
+     
+      System.out.println(connectionSocket.getInetAddress().toString()+ ":" + connectionSocket.getPort() + " message= "+str);  
+   
+      //Tool.pressEnterKeyToContinue("s1 ) premi enter chiudere la connessione con il client ");
+   
+      connectionSocket.close();
+      System.out.println("Ho chiuso la connessione !! con "+connectionSocket.getInetAddress().toString()+ ":" + connectionSocket.getPort() ) ;
+
+     } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
+     }
+  }
+
+
+  
 }
